3ee828e26202089d5d1ca0702d8e6c224f4e97bf
balbir <prasadbalbir1056@gmail.com>
2024-08-25 09:13:35 +0530

    fix: stringHandler

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -140,7 +140,7 @@ func stringHandler(lex *lexer, regex *regexp.Regexp) {
 	match := regex.FindStringIndex(lex.remainder()) 
 	stringLiteral := lex.remainder()[match[0]+1:match[1]-1]
 	lex.push(NewToken(STRING, stringLiteral))
-	lex.advanceN(len(stringLiteral))	 
+	lex.advanceN(len(stringLiteral)+2)	 
 	}
 
 func commentHandler(lex *lexer, regex *regexp.Regexp) {
